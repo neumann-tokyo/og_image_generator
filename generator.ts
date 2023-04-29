39714931fb1183b792b22f087f261ace2f3e1f53
kbaba1001 <kbaba1001@users.noreply.github.com>
2023-04-30 07:58:10 +0900

    fix: japanese font

--- generator.ts
+++ generator.ts
@@ -1,11 +1,9 @@
 import { createCanvas, loadImage } from "https://deno.land/x/canvas/mod.ts";
-import { type Image } from "https://deno.land/x/canvas@v1.4.1/mod.ts";
 
 const width = 1200;
 const height = 630;
 const imagePath = "./og-image-background.png";
-const font = "bold 70pt 'VL Gothic'";
-const title = "srucm輪講について"; // ここは引数で取る
+const title = "scrum輪講について"; // ここは引数で取る
 
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext("2d");
@@ -13,7 +11,9 @@ const backgroundImage = await loadImage(imagePath);
 
 ctx.drawImage(backgroundImage, 0, 0);
 
-ctx.font = font
+const font_buffer = await Deno.readFile('./VL-Gothic-Regular.ttf')
+canvas.loadFont(font_buffer, { family: 'VL-Gothic' })
+ctx.font = "70px VL-Gothic";
 ctx.fillStyle = "#000000";
 ctx.fillText(title, 50, 130);
 
